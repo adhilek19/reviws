da1fd31b78c78f0281442499e5f386a621395aaa
adhilek <saliek227@gmail.com>
2025-12-10 19:34:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Manirouter from './rout/Manirouter'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+<Manirouter/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/rout/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contact() {
+  return (
+  
+    <>
+    
+    
+    <h1>hiiiiii</h1>
+    
+    
+    </>
+  )
+}
+
+export default Contact
